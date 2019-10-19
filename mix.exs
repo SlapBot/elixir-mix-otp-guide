4534e218e12b52237fe7070bb35818198eebb01a
Ujjwal Gupta <ugupta41@gmail.com>
2019-10-19 12:49:50 +0530

    Topic: Umbrella Project implemented

--- mix.exs
+++ mix.exs
@@ -5,6 +5,10 @@ defmodule KV.MixProject do
     [
       app: :kv,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
